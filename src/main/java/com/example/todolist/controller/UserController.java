26a67ee1b0b2f6cc5888ff44a63917480f13e8ca
LenaNedorez <nedorezova.emp@gmail.com>
2022-06-12 19:47:49 +0300

    Add UserService

--- src/main/java/com/example/todolist/controller/UserController.java
+++ src/main/java/com/example/todolist/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.todolist.controller;
 
 import com.example.todolist.entity.UserEntity;
+import com.example.todolist.exception.UserAlreadyExistException;
 import com.example.todolist.repository.UserRepository;
+import com.example.todolist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,13 +13,15 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @PostMapping
     public ResponseEntity registration(@RequestBody UserEntity user) {
         try {
-            userRepository.save(user);
+            userService.registration(user);
             return ResponseEntity.ok("Пользователь был успешно сохранен.");
+        } catch(UserAlreadyExistException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         } catch(Exception e) {
             return ResponseEntity.badRequest().body("Произошла ошибка!");
         }
